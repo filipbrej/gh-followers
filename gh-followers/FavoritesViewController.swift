e0faacb7ad504a2a4e392568b0cdffda797152a3
Filip Brej <brej.filip@gmail.com>
2020-02-09 18:23:20 -0600

    Initial Commit

---
+++ gh-followers/FavoritesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesViewController.swift
+//  gh-followers
+//
+//  Created by Filip Brej on 2/9/20.
+//  Copyright © 2020 Filip Brej. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ gh-followers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  gh-followers
+//
+//  Created by Filip Brej on 2/9/20.
+//  Copyright © 2020 Filip Brej. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
