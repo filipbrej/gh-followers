403c168107a31266a070edb104d196b06a8b8cdf
Filip Brej <brej.filip@gmail.com>
2020-02-10 19:53:51 -0600

    Created custom views for buttons and textfield.

---
+++ gh-followers/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  gh-followers
+//
+//  Created by Filip Brej on 2/10/20.
+//  Copyright © 2020 Filip Brej. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // Button properties
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    // Further button customization
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
