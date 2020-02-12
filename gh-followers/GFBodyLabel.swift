a2b42404a203974469824c815c188fe4310f7d82
Filip Brej <brej.filip@gmail.com>
2020-02-12 12:10:06 -0600

    Created custom labels.

---
+++ gh-followers/GFBodyLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GFBodyLabel.swift
+//  gh-followers
+//
+//  Created by Filip Brej on 2/12/20.
+//  Copyright © 2020 Filip Brej. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlign: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
