c9c8cb4990ea49ff6e035da91b1f60ed4bcbb3fd
Filip Brej <brej.filip@gmail.com>
2020-02-17 20:57:04 -0600

    Created flow layout for collection view in follower list.

--- gh-followers/Controllers/FollowerListViewController.swift
+++ gh-followers/Controllers/FollowerListViewController.swift
@@ -11,28 +11,60 @@ import UIKit
 class FollowerListViewController: UIViewController {
 
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        configureViewController()
+        getFollowers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    func getFollowers() {
         // Make call to network manager
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
-                
             case .success(let followers):
                 print(followers)
-                
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "OK")
             }
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
+    func configureCollectionView() {
+        // initialize collection view
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+    
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.cellID)
+    }
+    
+    func createFlowLayout() -> UICollectionViewFlowLayout {
+        // Set up cell size
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        // Set up flow layout
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
 }
