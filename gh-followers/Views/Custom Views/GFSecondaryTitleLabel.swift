671d537246e74c070375957b3382037449cbb27f
Filip Brej <brej.filip@gmail.com>
2020-02-24 21:28:20 -0600

    Added custom label.

---
+++ gh-followers/Views/Custom Views/GFSecondaryTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  gh-followers
+//
+//  Created by Filip Brej on 2/24/20.
+//  Copyright © 2020 Filip Brej. All rights reserved.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(size: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: size, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
