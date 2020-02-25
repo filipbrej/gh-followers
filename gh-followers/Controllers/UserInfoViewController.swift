45909ef675f622351037d3f479637af41ecd611f
Filip Brej <brej.filip@gmail.com>
2020-02-24 21:27:24 -0600

    Added child view controller for user info

--- gh-followers/Controllers/UserInfoViewController.swift
+++ gh-followers/Controllers/UserInfoViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
     
+    let headerView = UIView()
+    
     var username: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -23,14 +25,36 @@ class UserInfoViewController: UIViewController {
             
             switch result {
             case .success(let user):
+                DispatchQueue.main.async {
+                    self.addChildVC(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
+                }
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
             }
         }
+        layoutUI()
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func addChildVC(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
     
     @objc func dismissVC() {
         dismiss(animated: true)
     }
-
+    
 }
