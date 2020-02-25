22d064120a038b3dbedf4c6a46523494c6871664
Filip Brej <brej.filip@gmail.com>
2020-02-24 21:28:02 -0600

    Added file to hold constants.

---
+++ gh-followers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  gh-followers
+//
+//  Created by Filip Brej on 2/24/20.
+//  Copyright © 2020 Filip Brej. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
