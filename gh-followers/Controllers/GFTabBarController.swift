7c096d6e08297225dc17942c209ff5c6e7ac4a94
Filip Brej <brej.filip@gmail.com>
2020-03-17 22:54:03 -0500

    Moved tab bar controller to new file.

---
+++ gh-followers/Controllers/GFTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBarController.swift
+//  gh-followers
+//
+//  Created by Filip Brej on 2/28/20.
+//  Copyright © 2020 Filip Brej. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    // Create tab navigation for search view controller
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    // Create tab navigation for favorites view controller
+    func createFavoritesNC()  -> UINavigationController {
+        let favoritesVC = FavoritesViewController()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+}
